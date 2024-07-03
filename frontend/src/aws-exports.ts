796ce27f39e9b57261a9048e9900ee46b866f745
Samuel Finder <Samuel.Finder@bbd.co.za>
2024-07-03 13:01:30 +0200

    changed package lock

---
+++ frontend/src/aws-exports.ts
@@ -0,0 +1,8 @@
+const awsExports = {
+	aws_project_region: "eu-west-1",
+	aws_cognito_region: "eu-west-1",
+	aws_user_pools_id: "eu-west-1_rCNlj6KY4",
+	aws_user_pools_web_client_id: "1fvja6sraqn6h850r4n0i1mjtf",
+};
+
+export default awsExports;
